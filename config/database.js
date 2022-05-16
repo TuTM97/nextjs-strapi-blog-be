837632ce0c107a98152ae406d1f8603375b3f31d
tutm8197 <tutm8197@gmail.com>
2022-05-16 09:49:03 +0700

    Config database to postgresql on heroku

--- config/database.js
+++ config/database.js
@@ -1,11 +1,21 @@
 const path = require('path');
 
+// strapi-api/config/database.js
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: env("DATABASE_HOST", "ec2-3-224-164-189.compute-1.amazonaws.com"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "dcaqnop5cod330"),
+        username: env("DATABASE_USERNAME", "nkxeibbjfddlat"),
+        password: env("DATABASE_PASSWORD", "5082ab22b6269d36f09af30825f88815ee97f1310c9b1dfbc1c7d5aec201acb7"),
+        schema: env("DATABASE_SCHEMA", "public"),
+      },
+      options: {},
     },
-    useNullAsDefault: true,
   },
 });
